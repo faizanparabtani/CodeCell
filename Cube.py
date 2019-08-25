800faae4f4131f0e69c7aa7ae2177bda50ecec51
faizanparabtani <52961945+faizanparabtani@users.noreply.github.com>
2019-08-25 21:56:53 +0530

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -1,11 +1,8 @@
 def cube(a):
-        c = a*a*a
-        print(c)
-        if (c % 2  == 0 and c % 4 != 0) or (c % 5 == 0 and c % 10 != 0) or (c % 8 == 0 and c % 4 != 0):
-            return 8
-        elif c % 10 == 0 or c % 4 == 0 or c % 7 == 0:
-            return 1
-        elif c % 3 == 0 or c % 6 == 0 or c % 9 == 0:
-            return 9
-        else:
-            return 'invalid input'
+    c = a*a*a
+    if c % 3 == 0:
+        return 9
+    elif c % 4 == 0 or c % 10 == 0 or c % 7 == 0:
+        return 1
+    else:
+        return 9
