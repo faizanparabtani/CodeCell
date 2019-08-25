4848497c58e5b4186ac70cfebb85fad2f52a7d17
faizanparabtani <52961945+faizanparabtani@users.noreply.github.com>
2019-08-25 22:20:47 +0530

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -1,8 +1,5 @@
 def cube(a):
-    c = a*a*a
-    if c % 3 == 0:
-        return 9
-    elif c % 4 == 0 or c % 10 == 0 or c % 7 == 0:
+    if (a - 1) % 3 == 0:
         return 1
     else:
         return 9
