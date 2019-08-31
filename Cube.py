2df536412d0de2910d01074cdefe5d3d05986484
faizanparabtani <52961945+faizanparabtani@users.noreply.github.com>
2019-08-31 17:59:39 +0530

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -1,5 +1,14 @@
-def cube(a):
-    if (a - 1) % 3 == 0:
-        return 1
-    else:
-        return 9
+def cubes(a):
+    sum = 0
+    for i in range(1, a+1):
+        c = i*i*i
+        if c > 9:
+            if c % 3 == 0:
+                sum += 9
+            elif (c-1) % 3 == 0:
+                sum += 8
+            else:
+                sum += 1
+        else:
+            sum += c
+    return sum % 1000007
